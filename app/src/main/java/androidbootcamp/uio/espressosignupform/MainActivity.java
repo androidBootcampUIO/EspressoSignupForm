8adcec71818694c380e8fb84e0fb6b3fcdd8d6db
Sebastian Puente <sebas7pr@gmail.com>
2017-09-25 07:22:47 -0500

    Agrega calendario para fecha de nacimiento

--- app/src/main/java/androidbootcamp/uio/espressosignupform/MainActivity.java
+++ app/src/main/java/androidbootcamp/uio/espressosignupform/MainActivity.java
@@ -1,13 +1,56 @@
 package androidbootcamp.uio.espressosignupform;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends Activity {
+
+    private EditText calendarEditText;
+    private Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calendar = Calendar.getInstance();
+        calendarEditText = findViewById(R.id.editText_birthday);
+        final DatePickerDialog.OnDateSetListener datePicker = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, day);
+                updateLabel();
+            }
+        };
+
+        calendarEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(MainActivity.this, datePicker, calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH))
+                        .show();
+
+            }
+        });
+
     }
+
+    private void updateLabel() {
+        String dateFormat = "MM/dd/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(dateFormat, Locale.US);
+
+        calendarEditText.setText(sdf.format(calendar.getTime()));
+    }
+
+
 }
